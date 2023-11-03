b69cccd40c6a866110d0a60fff3287279e0a1422
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-03 13:12:06 +0530

    Add files via upload

---
+++ JavaProgramms/unocareermorningclass/ArmStrongNumber2.java
@@ -0,0 +1,25 @@
+package com.unocareermorningclass;
+
+public class ArmStrongNumber2 {
+	static boolean isArmStrong(int n) {
+		int ld,sum=0,t;t=n;
+		while(n != 0) {
+			ld = n % 10;
+			n = n / 10;// 153  15 1 0
+			sum = sum +ld * ld * ld;
+		}
+		if(sum == t) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static void main(String[] args) {
+		for(int i=1;i<=1000;i++) {
+			if(isArmStrong(i) ==true) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
