b76ac38a7bc4aecf3f4d3794eeb26c886134c273
Himakumar Tellaboina <139366126+himakumar-github@users.noreply.github.com>
2023-11-07 15:36:54 +0530

    Add files via upload

---
+++ JavaProgramms/unocareermorningclass/RecursionOdd.java
@@ -0,0 +1,13 @@
+package com.unocareermorningclass;
+
+public class RecursionOdd {
+	static int factorial(int num) {
+		if (num==1)return 1;
+		else {
+			return num+factorial(num-1);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(factorial(10));
+	}
+}
